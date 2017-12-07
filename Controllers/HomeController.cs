b09d323b31560a7893dbc32a34c4ae1728237ecc
Igor Music <igor.music@rbc.com>
2017-12-07 07:26:45 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ngLoginWeb.Controllers
+{
+ 
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View("NgApp");
+        }
+        
+    }
+}
